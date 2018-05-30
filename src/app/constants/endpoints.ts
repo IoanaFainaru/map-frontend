3b7bfc3e233c83bd7279d0fb85539b1fd3301c2e
Constantin Radu <constantin.radu@maersk.com>
2018-05-30 23:27:46 +0300

    added endpoints

---
+++ src/app/constants/endpoints.ts
@@ -0,0 +1,16 @@
+
+const API_URL = `localhost:8080/api`;
+
+export const API = {
+    getAllCities: `${API_URL}/city/all`,
+
+    getTop10Cities: `${API_URL}/city/top10`,
+
+    // /city/user/{id}
+    getUserCities: `${API_URL}/city/user/`,
+
+    // POST: /user - add user
+    // POST: /user/{{userId}}/city/{{cityCode}} - add a city to user's account
+    // DELETE: /user/{{userId}}/city/{{cityCode}} - delete a city from user's account
+    user: `${API_URL}/user`,
+};
