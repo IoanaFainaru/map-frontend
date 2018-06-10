00275fefdd5638df35cd506ef5e2f1b579215586
Constantin Radu <radu17constantin@gmail.com>
2018-06-11 01:54:01 +0300

    create userService with register method (user/company)

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { API } from '../../constants/endpoints';
+
+@Injectable()
+export class UserService {
+
+    headers: any;
+
+    constructor(
+        private http: HttpClient,
+    ) {
+        this.headers = new HttpHeaders()
+            .append('Content-Type', 'application/json')
+            .append('Access-Control-Allow-Origin', '*');
+    }
+
+
+    /**
+     * @desc add a user or company to the DB
+     * @param {any} user can be user or company type
+     * @returns {Observable<any>}
+     * @memberof UserService
+     */
+    public register(user): Observable<any> {
+
+        return Observable.create(observer => {
+            this.http.post(API.user, user, { headers: this.headers })
+                .subscribe(response => {
+                    observer.next(response);
+                    observer.complete();
+                }, err => {
+                    observer.error(err);
+                });
+        });
+
+    }
+
+}
